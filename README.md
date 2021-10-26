732dd1643c022370b273c0b83fdf63ba8c769a5f
Will Mason Moses <36004763+willmmoses@users.noreply.github.com>
2021-10-26 16:37:08 -0500

    Extracts data properly from SQL database. Currently, the command only grabs 1 group chat, instead of all messages for simplicity's sake. I have no desire to parse 250k+ messages at this point in time.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dsc-2010-project-01
+
+Timestamps are in milliseconds since 2001-01-01, with six significant figures of precision.
--- project.R
+++ project.R
@@ -2,9 +2,12 @@ library("RSQLite")
 
 reader <- function (){
   print("Reading file")
-  db <- dbConnect(RSQLite::SQLite(), "iMessage-Data.sqlite")
-  print(dbListTables(db))
-  print(dbListFields(db, 'Messages'))
+  # db <- dbConnect(RSQLite::SQLite(), "iMessage-Data.sqlite")
+  db <- dbConnect(RSQLite::SQLite(), "chat.db")
+  result <- dbSendQuery(db, "select m.handle_id, m.text, m.date from message m INNER JOIN chat_message_join cmj ON cmj.chat_id = 1 AND m.ROWID = cmj.message_id")
+  # print(dbFetch(res))
+  data <- dbFetch(result)
+  # print(dbFetch(res))
 }
 
 reader()
