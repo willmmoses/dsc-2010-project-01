ba9de55c588c21399bca27a5978dfee3bcbad9ec
Will Mason Moses <willmason@moses.com>
2021-10-26 18:49:01 -0500

    Now provides average message length per person.

--- project.R
+++ project.R
@@ -1,13 +1,21 @@
 library("RSQLite")
+library("dplyr")
+library("dbplyr")
 
 reader <- function (){
   print("Reading file")
   # db <- dbConnect(RSQLite::SQLite(), "iMessage-Data.sqlite")
   db <- dbConnect(RSQLite::SQLite(), "chat.db")
-  result <- dbSendQuery(db, "select m.handle_id, m.text, m.date from message m INNER JOIN chat_message_join cmj ON cmj.chat_id = 1 AND m.ROWID = cmj.message_id")
+  messages <- tbl(db, 'message')
+  # result <- dbSendQuery(db, "select m.handle_id, m.text, m.date from message m INNER JOIN chat_message_join cmj ON cmj.chat_id = 1 AND m.ROWID = cmj.message_id")
   # print(dbFetch(res))
-  data <- dbFetch(result)
+  # data <- dbFetch(result)
   # print(dbFetch(res))
+  messages %>%
+    group_by(handle_id) %>%
+    summarize(Avg_Message_Length = mean(nchar(text))) %>%
+    collect()
+  # filter(handle_id = '1')
 }
 
 reader()
