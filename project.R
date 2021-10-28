5d459b4ab067f70d3675670bc7f2ec96a7828cbb
Will Mason Moses <36004763+willmmoses@users.noreply.github.com>
2021-10-28 13:48:31 -0500

    Now filters to individual words.

--- project.R
+++ project.R
@@ -1,7 +1,10 @@
 library("RSQLite")
+library("RMariaDB")
 library("dplyr")
 library("dbplyr")
 library("lubridate")
+library("stringr")
+library("stringi")
 
 reader <- function() {
   print("Reading file")
@@ -10,10 +13,13 @@ reader <- function() {
   my_messages <- messages %>%
     filter(handle_id == 0 & !is.na(text)) %>%
     mutate(epoch_date = (date/1000000) + 978307200000) %>% #reduces to miliseconds, adds offset between Core Date and Unix Epoch
-    select(text, epoch_date) %>%
     arrange(desc(epoch_date)) %>%
+    collect() %>%
+    mutate(split_text = str_split(text, "[^[A-Za-z0-9]]")) %>%
+    select(split_text -matches(""), epoch_date) %>%
     collect()
-  print(my_messages)
+
+  print(my_messages$split_text[1])
   dbDisconnect(db)
 }
 
