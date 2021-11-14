56956946e2f6f0b0008f2dfff69e5598f796cb2e
willmmoses <36004763+willmmoses@users.noreply.github.com>
2021-11-13 19:26:53 -0600

    Now displays bar plot of top 10 senders. Need to add more stats stuff still.

--- project.R
+++ project.R
@@ -10,20 +10,28 @@ reader <- function() {
 
 tidy_up <- function(messages) {
   filtered_me <- messages %>%
-    select(text, date, sender, room_name) %>%
-    mutate(formatted_date = as_datetime(date)) %>%
-    mutate(wday = wday(formatted_date))
+    select(text, date, id, is_from_me, room_name) %>%
+    mutate(formatted_date = as_datetime(date / 1000)) %>%
+    mutate(wday = wday(formatted_date)) %>%
+    mutate(id = case_when(is_from_me == 1 ~ '+16158308574', is_from_me == 0 ~ id))
   return(filtered_me)
 }
 
 
 stats <- function(messages) {
-
+  by_person <- messages %>%
+    filter(!id == "")
+  by_id <- table(by_person$id)
+  by_id <- sort(by_id, decreasing = TRUE)
+  by_id <- head(by_id, 100)
+  barplot(head(by_id,10), xlab = 'Sender', ylab = 'Number of Messages Sent')
+  print(head(by_id))
 }
 
 main <- function() {
   messages <- reader()
   filtered_me <- tidy_up(messages)
+  stats(filtered_me)
 }
 
 main()
