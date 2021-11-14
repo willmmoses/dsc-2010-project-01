c1c61b2b1b4dbe1864834ea3bc8da705d9101dbe
willmmoses <36004763+willmmoses@users.noreply.github.com>
2021-11-14 16:35:57 -0600

    Added day of week filtering and friend group chat stats.

--- project.R
+++ project.R
@@ -10,28 +10,50 @@ reader <- function() {
 
 tidy_up <- function(messages) {
   filtered_me <- messages %>%
-    select(text, date, id, is_from_me, room_name) %>%
+    select(text, date, id, is_from_me, chat_guid) %>%
     mutate(formatted_date = as_datetime(date / 1000)) %>%
     mutate(wday = wday(formatted_date)) %>%
-    mutate(id = case_when(is_from_me == 1 ~ '+16158308574', is_from_me == 0 ~ id))
+    mutate(id = case_when(is_from_me == 1 ~ '+16158308574', is_from_me == 0 ~ id)) %>%
+    filter(!id == "")
   return(filtered_me)
 }
 
 
-stats <- function(messages) {
-  by_person <- messages %>%
-    filter(!id == "")
-  by_id <- table(by_person$id)
+top10 <- function(messages) {
+  by_id <- table(messages$id)
   by_id <- sort(by_id, decreasing = TRUE)
   by_id <- head(by_id, 100)
-  barplot(head(by_id,10), xlab = 'Sender', ylab = 'Number of Messages Sent')
+  with_me <- barplot(head(by_id, 10), ylim = c(0, 100000), xlab = 'Sender', ylab = 'Number of Messages Sent')
+  with_me_y <- as.matrix(head(by_id, 10))
+  text(with_me, with_me_y + 3000, labels = as.character(with_me_y))
+  without_me <- barplot(tail(head(by_id, 11), 10), ylim = c(0, 30000), xlab = 'Sender', ylab = 'Number of Messages Sent')
+  without_me_y <- as.matrix(tail(head(by_id, 11), 10))
+  text(without_me, without_me_y + 1000, labels = as.character(without_me_y))
   print(head(by_id))
+}
+
+sprangbreak <- function(messages) {
+  just_chat <- messages %>%
+    filter(chat_guid == 'iMessage;+;chat83626697018543747')
+  by_id <- table(just_chat$id)
+  by_id <- sort(by_id, decreasing = TRUE)
+  with_me <- barplot(by_id, ylim = c(0, 18000), xlab = 'Sender', ylab = 'Number of Messages Sent in Chat per Person')
+  with_me_y <- as.matrix(by_id)
+  text(with_me, with_me_y + 500, labels = as.character(with_me_y))
+  print(head(by_id))
+  by_day <- table(just_chat$wday)
+  # by_day <- sort(by_day, decreasing = TRUE)
+  with_me <- barplot(by_day, ylim = c(0, 12000), xlab = 'Day of Week', ylab = 'Number of Messages Sent in Chat per Day')
+  with_me_y <- as.matrix(by_day)
+  text(with_me, with_me_y + 500, labels = as.character(with_me_y))
+
 }
 
 main <- function() {
   messages <- reader()
   filtered_me <- tidy_up(messages)
-  stats(filtered_me)
+  # top10(filtered_me)
+  sprangbreak(filtered_me)
 }
 
 main()
