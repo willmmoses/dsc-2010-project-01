668362b8e440515a72e0e9c90268e0580125c8fe
willmmoses <36004763+willmmoses@users.noreply.github.com>
2021-11-29 14:16:36 -0600

    Added messages by day of week, and hypothesis test of sent vs received

--- project.R
+++ project.R
@@ -10,7 +10,7 @@ reader <- function() {
 
 tidy_up <- function(messages) {
   filtered_me <- messages %>%
-    select(text, date, id, is_from_me, chat_guid) %>%
+    select(text, date, id, is_from_me, chat_guid, cache_has_attachments, associated_message_guid, is_emote) %>%
     mutate(formatted_date = as_datetime(date / 1000)) %>%
     mutate(wday = wday(formatted_date)) %>%
     mutate(id = case_when(is_from_me == 1 ~ '+16158308574', is_from_me == 0 ~ id)) %>%
@@ -46,14 +46,41 @@ sprangbreak <- function(messages) {
   with_me <- barplot(by_day, ylim = c(0, 12000), xlab = 'Day of Week', ylab = 'Number of Messages Sent in Chat per Day')
   with_me_y <- as.matrix(by_day)
   text(with_me, with_me_y + 500, labels = as.character(with_me_y))
+}
 
+words <- function(messages) {
+  actual_text <- messages %>%
+    filter(is_emote == 0 & cache_has_attachments == 0 & id == '+16158308574')
+  actual_text <- table(actual_text$text)
+  print(actual_text)
+  just_words <- paste(actual_text, sep = ' ')
+  print(summary(just_words))
+  # print(just_words)
+  # split_words <- strsplit(actual_text," ", perl = TRUE)
+  # head(summary(split_words))
+}
+
+messages_by_day <- function(messages) {
+  actual_text <- messages %>%
+    filter(is_emote == 0 & cache_has_attachments == 0 & id == '+16158308574')
+  by_day <- table(actual_text$wday)
+  sent <- barplot(by_day, ylim = c(0, 18000), xlab = 'Day of Week', ylab = 'Number of Messages Sent per Day')
+  sent_y <- as.matrix(by_day)
+  text(sent, sent_y + 500, labels = as.character(sent_y))
+  actual_text <- messages %>%
+  filter(is_emote == 0 & cache_has_attachments == 0 & id != '+16158308574')
+  by_day <- table(actual_text$wday)
+  received <- barplot(by_day, ylim = c(0, 25000), xlab = 'Day of Week', ylab = 'Number of Messages Received per Day')
+  received_y <- as.matrix(by_day)
+  text(received, received_y + 500, labels = as.character(received_y))
+  cor.test(sent_y, received_y)
 }
 
 main <- function() {
   messages <- reader()
   filtered_me <- tidy_up(messages)
-  # top10(filtered_me)
-  sprangbreak(filtered_me)
+  top10(filtered_me)
+  messages_by_day(filtered_me)
 }
 
 main()
